c01cca502e26e493eb15bbecaeccb5223d11ecb4
anagaby2196 <80338672+anagaby2196@users.noreply.github.com>
2022-07-08 16:13:14 -0600

    Se crea la vista del cardview de servicios

---
+++ app/src/main/java/com/clinica_medica/adapter/ServicioAdapter.kt
@@ -0,0 +1,46 @@
+package com.clinica_medica.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.clinica_medica.databinding.ServicioFilaBinding
+import com.clinica_medica.model.Servicio
+import com.clinica_medica.ui.servicio.ServicioFragmentDirections
+
+class ServicioAdapter : RecyclerView.Adapter<ServicioAdapter.ServicioViewHolder>() {
+
+    private var listaServicios = emptyList<Servicio>()
+
+    inner class ServicioViewHolder(private val itemBinding: ServicioFilaBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+        fun dibuja(servicio : Servicio) {
+            itemBinding.tvNombreServicio.text = servicio.nombreServicio
+            itemBinding.tvDescripcionServicio.text = servicio.descripcion
+            itemBinding.tvCosto.text = servicio.costo.toString()
+
+//            itemBinding.vistaFila.setOnClickListener{
+//                val accion = ServicioFragmentDirections.actionNavClinicaToUpdateServicioFragment(servicio)
+//                itemView.findNavController().navigate(accion)
+//            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ServicioViewHolder {
+        val itemBinding = ServicioFilaBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ServicioViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: ServicioViewHolder, position: Int) {
+        val servicio = listaServicios[position]
+        holder.dibuja(servicio)
+    }
+
+    override fun getItemCount(): Int {
+        return listaServicios.size
+    }
+
+    fun setData(servicios : List<Servicio>) {
+        this.listaServicios = servicios
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/clinica_medica/ui/servicio/ServicioFragment.kt
+++ app/src/main/java/com/clinica_medica/ui/servicio/ServicioFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.clinica_medica.R
+import com.clinica_medica.adapter.ServicioAdapter
 import com.clinica_medica.databinding.FragmentServicioBinding
 import com.clinica_medica.viewmodel.ServicioViewModel
 
@@ -26,6 +28,19 @@ class ServicioFragment : Fragment() {
 
         binding.addServicioButton.setOnClickListener{
             findNavController().navigate(R.id.action_nav_servicio_to_addServicioFragment)
+        }
+
+        //Activar el reciclador - RecyclerView
+        val servicioAdapter = ServicioAdapter()
+        val recicladorServicio = binding.recicladorServicio
+
+        recicladorServicio.adapter = servicioAdapter
+        recicladorServicio.layoutManager = LinearLayoutManager(requireContext())
+
+        servicioViewModel = ViewModelProvider(this)[ServicioViewModel::class.java]
+
+        servicioViewModel.getAllData.observe(viewLifecycleOwner) {
+                servicios -> servicioAdapter.setData(servicios)
         }
 
         return binding.root
